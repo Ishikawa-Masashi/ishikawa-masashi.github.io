cedd973b3205d5a2750d38f0db6214a9562cb4d1
ishikawa-masashi <IshikawaMasashi@outlook.com>
2025-05-29 07:40:48 +0900

    fix

--- src/consts.ts
+++ src/consts.ts
@@ -6,4 +6,4 @@ export const SITE_DESCRIPTION = "Welcome to my website!";
 export const TWITTER_URL = "https://x.com/YnZRLsRXNK46265";
 export const GITHUB_URL = "https://github.com/Ishikawa-Masashi";
 export const EMAIL_ADDRESS = "oniku.udon.029@gmail.com";
-export const TEST_IMG = "/pasonoko.png";
+export const TEST_IMG = "/ishikawa.svg";
